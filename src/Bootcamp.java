42f77fd573a6273316168b0239582488fd0503df
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-06-12 10:12:00 -0300

    first commit

---
+++ src/Bootcamp.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Bootcamp {
+    public static void main(String[] args) {
+        String nome;
+        String descricao;
+        Date data_Inicial;
+        Date data_Final;
+    }
+}
